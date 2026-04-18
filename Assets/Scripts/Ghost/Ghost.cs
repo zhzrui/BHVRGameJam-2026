8e521405331bfb128d0e254209147b254cd67e1d
tab <58062648+tabithacatitha@users.noreply.github.com>
2026-04-18 00:00:12 -0400

    clickable ghost idiots

--- Assets/Scripts/Ghost/Ghost.cs
+++ Assets/Scripts/Ghost/Ghost.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Ghost : MonoBehaviour
+public class Ghost : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 {
     [Header("Variables")]
-    [SerializeField] float timeLeft = 10.0f;
+    [SerializeField] float clickTime = 3.0f;
     [SerializeField] float explodeTime = 2.0f;
-    [SerializeField] bool clicked = false;
+    private float timeLeft;
+    private float explodeTimeLeft;
+    private bool clicked = false;
     private Renderer rend;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rend = GetComponent<Renderer>();
+        timeLeft = clickTime;
+        explodeTimeLeft = explodeTime;
     }
 
     void Update()
     {
-        if (clicked && timeLeft > 0)
+        if (clicked && timeLeft > 0 && explodeTimeLeft == explodeTime)
         {
             rend.material.SetInt("_isClicking", 1);
             timeLeft -= Time.deltaTime;
@@ -34,13 +39,36 @@ public class Ghost : MonoBehaviour
     {
         rend.material.SetInt("_isExploding", 1);
         rend.material.SetInt("_isClicking", 0);
-        while (explodeTime > 0)
+        while (explodeTimeLeft > 0)
         {
             yield return new WaitForEndOfFrame();
-            explodeTime -= Time.deltaTime;
-            rend.material.SetFloat("_explodeTime", explodeTime);
+            explodeTimeLeft -= Time.deltaTime;
+            rend.material.SetFloat("_explodeTime", explodeTimeLeft);
         }
 
         Destroy(this.gameObject);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        clicked = true;
+        Debug.Log("clicked");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        clicked = false;
+        timeLeft = clickTime;
+        Debug.Log("unclicked");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (clicked)
+        {
+            clicked = false;
+            timeLeft = clickTime;
+            Debug.Log("unclicked");
+        }
     }
 }
