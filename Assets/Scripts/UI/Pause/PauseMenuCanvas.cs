d12689ba52fe0643615639d116aa4ea37b7a4843
tab <58062648+tabithacatitha@users.noreply.github.com>
2026-04-19 02:10:14 -0400

    paws menu

--- Assets/Scripts/UI/Pause/PauseMenuCanvas.cs
+++ Assets/Scripts/UI/Pause/PauseMenuCanvas.cs
@@ -44,7 +44,7 @@ public class PauseMenu : MonoBehaviour
             canvasGroup.blocksRaycasts = true;
             canvasGroup.interactable = true;
             Time.timeScale = 0f;
-            DOTween.To(()=> volume.weight, x=> volume.weight = x, 1.0f, 0.5f);
+            DOTween.To(()=> volume.weight, x=> volume.weight = x, 1.0f, 0.5f).SetUpdate(true);
             canvasGroup.DOFade(1f, 0.5f).SetUpdate(true);
             // canvasGroup.DOFade(1f, 0.5f).OnComplete(() => { Time.timeScale = 0f; });
         }
@@ -57,7 +57,7 @@ public class PauseMenu : MonoBehaviour
             menuShown = false;
             canvasGroup.blocksRaycasts = false;
             canvasGroup.interactable = false;
-            DOTween.To(()=> volume.weight, x=> volume.weight = x, 0.0f, 0.5f);
+            DOTween.To(()=> volume.weight, x=> volume.weight = x, 0.0f, 0.5f).SetUpdate(true);
             canvasGroup.DOFade(0f, 0.5f).SetUpdate(true).OnComplete(() => { Time.timeScale = 1f; });
         }
     }
